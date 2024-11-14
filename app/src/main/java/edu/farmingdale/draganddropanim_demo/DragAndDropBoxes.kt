c365f34772827d1600432b86af51dd3e6c27e0c3
Moaath Alrajab <alrajam@farmingdale.edu>
2024-11-14 10:03:50 -0500

    Init commit

---
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
@@ -0,0 +1,128 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package edu.farmingdale.draganddropanim_demo
+
+import android.content.ClipData
+import android.content.ClipDescription
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.draganddrop.dragAndDropSource
+import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.DragAndDropTransferData
+import androidx.compose.ui.draganddrop.mimeTypes
+import androidx.compose.foundation.Canvas
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun DragAndDropBoxes(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth().weight(0.2f)
+        ) {
+            val boxCount = 4
+            var dragBoxIndex by remember {
+                mutableIntStateOf(0)
+            }
+
+            repeat(boxCount) { index ->
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxHeight()
+                        .padding(10.dp)
+                        .border(1.dp, Color.Black)
+                        .dragAndDropTarget(
+                            shouldStartDragAndDrop = { event ->
+                                event
+                                    .mimeTypes()
+                                    .contains(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                            },
+                            target = remember {
+                                object : DragAndDropTarget {
+                                    override fun onDrop(event: DragAndDropEvent): Boolean {
+
+                                        dragBoxIndex = index
+                                        return true
+                                    }
+                                }
+                            }
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    this@Row.AnimatedVisibility(
+                        visible = index == dragBoxIndex,
+                        enter = scaleIn() + fadeIn(),
+                        exit = scaleOut() + fadeOut()
+                    ) {
+                        Text(
+                            text = "Right",
+                            fontSize = 40.sp,
+                            color = Color.Red,
+                            fontWeight = FontWeight.Bold,
+
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .dragAndDropSource {
+                                    detectTapGestures(
+                                        onLongPress = { offset ->
+                                            startTransfer(
+                                                transferData = DragAndDropTransferData(
+                                                    clipData = ClipData.newPlainText(
+                                                        "text",
+                                                        ""
+                                                    )
+                                                )
+                                            )
+                                        }
+                                    )
+                                }
+                        )
+                    }
+                }
+            }
+        }
+
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.8f)
+                .background(Color.Red)
+
+        ) {
+                drawCircle(Color.Green, radius = 50f, center = Offset(100f, 100f))
+       }
+    }
+}
+
---
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -0,0 +1,58 @@
+package edu.farmingdale.draganddropanim_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.farmingdale.draganddropanim_demo.ui.theme.DragAndDropAnim_DemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DragAndDropAnim_DemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   Greeting("Android", Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
+
+
+
+// This should be completed in a group setting
+// ToDo 1: Analyze the requirements for Individual Project 3
+// ToDo 2: Show the DragAndDropBoxes composable
+// ToDo 3: Change the circle to a rect
+// ToDo 4: Replace the command right with a image or icon
+// ToDo 5: Make this works in landscape mode only
+// ToDo 6: Rotate the rect around itself
+// ToDo 7: Move - translate the rect horizontally and vertically
+// ToDo 8: Add a button to reset the rect to the center of the screen
+// ToDo 9: Enable certain animation based on the drop event (like up or down)
+// ToDo 10: Make sure to commit for each one of the above and submit this individually
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DragAndDropAnim_DemoTheme {
+        Greeting("Android")
+    }
+}
